263e92d36f38b90deb933e3f51ee41150f6dce35
Ondrej Zdych <zdych.ondrej@gmail.com>
2017-07-31 18:22:37 +0200

    feat: add REPLnode

---
+++ src/repl.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const repl = require('repl');
+const touch = require('touch');
+
+const Lexer = require('./lexer');
+const { TokenType } = require('./token');
+
+const REPL_HISTORY_FILE = '.repl_history';
+
+function lex (input, context, filename, callback) { // eslint-disable-line max-params
+  const lexer = new Lexer(input);
+  const output = [];
+
+  let token = null;
+
+  do {
+    token = lexer.nextToken();
+
+    output.push(token);
+  }
+  while (token.type !== TokenType.EOF);
+
+  return callback(
+    output.map((t) => JSON.stringify(t, null, 2))
+  );
+}
+
+// create REPL server
+const server = repl.start({
+  prompt: '>> ',
+  eval: lex,
+});
+
+touch.sync(REPL_HISTORY_FILE);
+
+// load command history from a file
+fs.readFileSync(REPL_HISTORY_FILE, 'utf8')
+  .split('\n')
+  .reverse()
+  .filter((line) => line.trim())
+  .map((line) => server.history.push(line));
+
+// save command history
+server.on('exit', () => {
+  fs.appendFileSync(REPL_HISTORY_FILE, server.lines.join('\n'));
+});
