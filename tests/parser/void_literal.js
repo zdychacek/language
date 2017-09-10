f8d53ad4d2c583d8922f702ca55380f9cce8bd4a
Ondrej Zdych <zdych.ondrej@gmail.com>
2017-09-10 15:31:27 +0200

    test: add tests for void literal

---
+++ tests/parser/void_literal.js
@@ -0,0 +1,23 @@
+import test from 'tape';
+
+import Lexer from '../../src/lexer/lexer';
+import Parser from '../../src/parser/parser';
+import * as ast from '../../src/parser/ast';
+import { is } from '../utils';
+
+test('Parser - Void expression', (t) => {
+  const input = 'void';
+
+  const lexer = new Lexer(input);
+  const parser = new Parser(lexer);
+  let program = null;
+
+  t.doesNotThrow(() => program = parser.parseProgram(), 'There are no parsing errors.');
+
+  const stmt = program.statements[0];
+
+  t.ok(is(stmt.expression, ast.VoidLiteral), 'node is VoidLiteral');
+  t.equal(stmt.expression.literal, 'void', 'literal value is "void"');
+
+  t.end();
+});
